35a283772715c195699fa7808f60e124817da049
unknown <freekodelka86@gmail.com>
2023-04-13 21:33:50 +0300

    Решена задача 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,7 +1,24 @@
-﻿Напишите программу, которая принимает на вход 
+﻿/*Напишите программу, которая принимает на вход 
 трёхзначное число и на выходе показывает 
 вторую цифру этого числа.
 456 -> 5
 782 -> 8
-918 -> 1
+918 -> 1*/
+
+int number = new Random().Next (99,1000);
+Console.WriteLine (number);
+int del3 = number /=10;
+int del1 = number % 10;
+Console.WriteLine(del1);
+
+
+
+/*int del1 = number / 100;
+Console.WriteLine(del1);
+
+Console.WriteLine();*/
+
+
+
+
 
