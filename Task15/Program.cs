4039afffc584bafaebdb4b5db5cd7bbe2ac44fd1
unknown <freekodelka86@gmail.com>
2023-04-14 21:23:30 +0300

    задача 15 решена

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, 
+которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+Не много нубски, но по другому я не сообразил*/
+
+
+int number = new Random().Next (0,10000);
+Console.WriteLine (number);
+
+while (number > 999)
+{
+    number /=10;
+}
+
+if (number < 100)
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
+else {
+    int third_number = number % 10;
+    Console.WriteLine (third_number);
+}
+
+
+
+
+
